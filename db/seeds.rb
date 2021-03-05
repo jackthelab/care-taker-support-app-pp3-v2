2ea9ae5256497314bee3bf804a41a98060b45e94
Joy Evans <joyevans@me.com>
2021-03-05 15:21:40 -0600

    seed data on support_groups

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+support1 = SupportGroup.create(name: "Adult Children of Alcoholics", topic: "n", meeting_day: "Sunday", location: "Houston,TX")
+support1 = SupportGroup.create(name: "Alcoholics Anonymous", topic: "n", meeting_day: "Monday", location: "Houston,TX")
+support1 = SupportGroup.create(name: "Alzheimer's Association", topic: "n", meeting_day: "Tuesday", location: "Houston,TX")
+support1 = SupportGroup.create(name: "Autism Society of America", topic: "n", meeting_day: "Wednesday", location: "Houston,TX")
+support1 = SupportGroup.create(name: "Caregiver Action Network", topic: "n", meeting_day: "Thursday", location: "Houston,TX")
+support1 = SupportGroup.create(name: "The Compassionate Friends Grief Support", topic: "n", meeting_day: "Friday", location: "Houston,TX")
+support1 = SupportGroup.create(name: "Crisis Text Line", topic: "n", meeting_day: "Saturday", location: "Houston,TX")
+support1 = SupportGroup.create(name: "Emotions Anonymous", topic: "n", meeting_day: "Sunday", location: "Houston,TX")
+support1 = SupportGroup.create(name: "Gamblers Anonymous", topic: "n", meeting_day: "Monday", location: "Houston,TX")
+support1 = SupportGroup.create(name: "Well Spouse Association", topic: "n", meeting_day: "Tuesday", location: "Houston,TX")
