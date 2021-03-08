c04043cd426637e1054ab8e7b46f679d77bf0d7a
Jacob Hampton <jhampton792@gmail.com>
2021-03-08 10:05:31 -0600

    Added constants for urls

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,9 @@
+const BASE_URL = 'http://localhost:3000/'
+const MEMBERS_URL = BASE_URL + 'members/'
+const SUPPORT_GROUPS_URL = BASE_URL + 'support_groups/'
+const CHECK_INS_URL = BASE_URL + 'check_ins/'
+
 document.addEventListener("DOMContentLoaded", () => {
     console.log("connected")
 })
+
